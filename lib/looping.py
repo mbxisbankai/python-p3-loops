918f98fd6b2c301afb95c26c5940444b31038765
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-05-03 11:29:04 +0300

    Python Loops Lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    for i in range(10, 0, -1):
+        print(i)
+    print('Happy New Year!')
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    int_list = [int ** 2 for int in int_list]
+    return int_list
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range (1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
